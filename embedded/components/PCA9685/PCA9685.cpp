d004d1ae605c596d0adf755ad07561de886d23d5
Enes <anas.abk2001@gmail.com>
2023-05-01 15:04:24 +0300

    Update PCA9685.cpp

--- embedded/components/PCA9685/PCA9685.cpp
+++ embedded/components/PCA9685/PCA9685.cpp
@@ -153,6 +153,6 @@ void PCA9685::set_pwm_us(const int channel, const double us) {
 	auto period_us = 1000000.0 / frequency;
 	auto bits_per_us = 4096 / period_us;
 	auto bits = us * bits_per_us;
-	printf("%d\n", bits);
+	printf("%f\n", bits);
 	set_pwm(channel, 0, bits);
 }
