af22925a1bd49980e1e7206fbba73815980ab5c9
Enes <anas.abk2001@gmail.com>
2023-05-01 15:13:45 +0300

    Update PCA9685.cpp

--- embedded/components/PCA9685/PCA9685.cpp
+++ embedded/components/PCA9685/PCA9685.cpp
@@ -150,11 +150,9 @@ int PCA9685::get_pwm(uint8_t led){
 }
 
 void PCA9685::set_pwm_us(const int channel, const int us) {
-	// auto period_us = 1000000.0 / frequency;
-	// auto bits_per_us = 4096 / period_us;
-	// auto bits = us * bits_per_us;
-	// printf("%f\n", bits);
-	uint32_t bits = 4096 * us * frequency / 1000000;
-	printf("%d\n", bits);
+	double period_us = 1000000.0 / frequency;
+	int bits_per_us = 4096 / period_us;
+	int bits = us * bits_per_us;
+	printf("%f %d %d\n", period_us, bits_per_us, bits);
 	set_pwm(channel, 0, bits);
 }
