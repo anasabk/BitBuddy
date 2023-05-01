5b59b40c798d6275315b6326ca06c1c89194c781
Enes <anas.abk2001@gmail.com>
2023-05-01 15:03:08 +0300

    trying to fix data types

--- embedded/components/PCA9685/PCA9685.cpp
+++ embedded/components/PCA9685/PCA9685.cpp
@@ -149,7 +149,7 @@ int PCA9685::get_pwm(uint8_t led){
 	return ledval;
 }
 
-void PCA9685::set_pwm_us(const int channel, const int us) {
+void PCA9685::set_pwm_us(const int channel, const double us) {
 	auto period_us = 1000000.0 / frequency;
 	auto bits_per_us = 4096 / period_us;
 	auto bits = us * bits_per_us;
