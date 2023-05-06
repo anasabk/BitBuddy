8d6509997eea01d238366b77fb00e61e9a8bc941
Enes <anas.abk2001@gmail.com>
2023-05-06 22:48:51 +0300

    Update main.cpp

--- embedded/main.cpp
+++ embedded/main.cpp
@@ -126,46 +126,46 @@ int main() {
     // }
 
 
-	// if (gpioInitialise() < 0) {
-	// 	printf("Failure...");
-	// 	exit(-1);
-	// }
-	// MPU6050 device(1, 0x68);
-	// MPU6050::MPU6050_data_t data;
-	// float ax, ay, az, gr, gp, gy; //Variables to store the accel, gyro and angle values
+	if (gpioInitialise() < 0) {
+		printf("Failure...");
+		exit(-1);
+	}
+	MPU6050 device(1, 0x68);
+	MPU6050::MPU6050_data_t data;
+	float ax, ay, az, gr, gp, gy; //Variables to store the accel, gyro and angle values
 
-	// sleep(1); //Wait for the MPU6050 to stabilize
+	sleep(1); //Wait for the MPU6050 to stabilize
 
-	// /*
-	// //Calculate the offsets
-	// std::cout << "Calculating the offsets...\n    Please keep the accelerometer level and still\n    This could take a couple of minutes...";
-	// device.getOffsets(&ax, &ay, &az, &gr, &gp, &gy);
-	// std::cout << "Gyroscope R,P,Y: " << gr << "," << gp << "," << gy << "\nAccelerometer X,Y,Z: " << ax << "," << ay << "," << az << "\n";
-	// */
+	/*
+	//Calculate the offsets
+	std::cout << "Calculating the offsets...\n    Please keep the accelerometer level and still\n    This could take a couple of minutes...";
+	device.getOffsets(&ax, &ay, &az, &gr, &gp, &gy);
+	std::cout << "Gyroscope R,P,Y: " << gr << "," << gp << "," << gy << "\nAccelerometer X,Y,Z: " << ax << "," << ay << "," << az << "\n";
+	*/
 
-	// //Read the current yaw angle
-	// device.calc_yaw = true;
+	//Read the current yaw angle
+	device.calc_yaw = true;
 
-	// while(1) {
-	// 	// device.getAngle(0, &gr);
-	// 	// device.getAngle(1, &gp);
-	// 	// device.getAngle(2, &gy);
+	while(1) {
+		// device.getAngle(0, &gr);
+		// device.getAngle(1, &gp);
+		// device.getAngle(2, &gy);
 
-	// 	// std::cout << "Current angle around the roll axis: " << gr << "\n";
-	// 	// std::cout << "Current angle around the pitch axis: " << gp << "\n";
-	// 	// std::cout << "Current angle around the yaw axis: " << gy << "\n";
+		// std::cout << "Current angle around the roll axis: " << gr << "\n";
+		// std::cout << "Current angle around the pitch axis: " << gp << "\n";
+		// std::cout << "Current angle around the yaw axis: " << gy << "\n";
 
-	// 	device.read_data(&data);
-	// 	printf("Accel x: %.3f, y: %.3f, z: %.3f / Gyro x: %3.f, y: %3.f, z: %3.f\n", 
-	// 		data.x_accel, 
-	// 		data.y_accel,
-	// 		data.z_accel,
-	// 		data.x_rot,
-	// 		data.y_rot,
-	// 		data.z_rot
-	// 	);
-	// 	usleep(500000); //0.25sec
-	// }
+		device.read_data(&data);
+		printf("Accel x: %.3f, y: %.3f, z: %.3f / Gyro x: %3.f, y: %3.f, z: %3.f\n", 
+			data.x_accel, 
+			data.y_accel,
+			data.z_accel,
+			data.x_rot,
+			data.y_rot,
+			data.z_rot
+		);
+		usleep(500000); //0.25sec
+	}
 
 	// //Get the current accelerometer values
 	// device.getAccel(&ax, &ay, &az);
@@ -175,21 +175,21 @@ int main() {
 	// device.getGyro(&gr, &gp, &gy);
 	// std::cout << "Gyroscope Readings: X: " << gr << ", Y: " << gp << ", Z: " << gy << "\n";
 
-	// gpioTerminate();
-	// return 0;
-
-	if (gpioInitialise() < 0) {
-		printf("Failure...");
-		exit(-1);
-	}
-
-	HC_SR04 sensor(6, 5);
-
-	while(1) {
-		printf("distance = %f\n", sensor.get_distance());
-		usleep(1000000);
-	}
-
 	gpioTerminate();
 	return 0;
+
+	// if (gpioInitialise() < 0) {
+	// 	printf("Failure...");
+	// 	exit(-1);
+	// }
+
+	// HC_SR04 sensor(6, 5);
+
+	// while(1) {
+	// 	printf("distance = %f\n", sensor.get_distance());
+	// 	usleep(1000000);
+	// }
+
+	// gpioTerminate();
+	// return 0;
 }
