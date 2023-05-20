56b32e7a9937dfe50a27236804bf39af3e52a97f
Emircan Demirel <e.demirel2019@gtu.edu.tr>
2023-05-20 14:13:18 +0300

    dataread - time problem - to be tested

--- embedded/components/MPU6050/dataread.cpp
+++ embedded/components/MPU6050/dataread.cpp
@@ -14,7 +14,7 @@ int main() {
         return 1;
     }
 
-    // Initalize
+    // Initialize
     MPU6050 mpu(1, I2C_ADDRESS);
 
     MPU6050::MPU6050_data_t data;
@@ -28,21 +28,34 @@ int main() {
 
     std::ofstream outputFile("sensorData.txt");
 
-    auto startTime = std::chrono::system_clock::now();
-    while (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - startTime).count() < 5) {
+    // Get current time
+    struct timespec timeNow;
+    clock_gettime(CLOCK_MONOTONIC, &timeNow);
+
+    while (true) {
         mpu.read_data(&data);
 
-        auto timeNow = std::chrono::system_clock::now();
-        std::time_t systemTime = std::chrono::system_clock::to_time_t(timeNow);
+        std::time_t systemTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 
         outputFile << "Time: " << std::ctime(&systemTime);
         outputFile << "AccelX: " << data.x_accel << ", AccelY: " << data.y_accel << ", AccelZ: " << data.z_accel << std::endl;
         outputFile << "GyroX: " << data.x_rot << ", GyroY: " << data.y_rot << ", GyroZ: " << data.z_rot << std::endl;
 
-        struct timespec sleepTime;
-        sleepTime.tv_sec = 0;
-        sleepTime.tv_nsec = 10000000; // 10 ms in nanoseconds
-        nanosleep(&sleepTime, nullptr); // 100Hz = 10ms delay
+        // Add 10ms to current time
+        timeNow.tv_nsec += 10000000L; // 10 ms in nanoseconds
+        // Handle overflow
+        while (timeNow.tv_nsec >= 1000000000L) {
+            timeNow.tv_nsec -= 1000000000L;
+            timeNow.tv_sec++;
+        }
+
+        // Sleep until the next 10ms point
+        clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &timeNow, nullptr);
+
+        // Check if 5 seconds have passed since the start
+        if (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - std::chrono::system_clock::from_time_t(systemTime)).count() >= 5) {
+            break;
+        }
     }
 
     outputFile.close();
