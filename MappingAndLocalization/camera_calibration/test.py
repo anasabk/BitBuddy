7d9449592f6ec7061d082153cab3401b536125b2
anasabk <anas.abk2001@gmail.com>
2023-05-25 14:15:07 +0100

    calibration update

--- MappingAndLocalization/camera_calibration/test.py
+++ MappingAndLocalization/camera_calibration/test.py
@@ -1,7 +1,7 @@
 import cv2
 import cv2.aruco as aruco
 import numpy as np
-from imutils.video import WebcamVideoStream
+from imutils.video import VideoStream
 import imutils
 
 import time
@@ -12,7 +12,7 @@ import sys
 
 width=640
 height=480
-cap = WebcamVideoStream(src=0, height=height, width=width).start()
+cap = VideoStream(src=0).start()
 viewVideo=True
 if len(sys.argv)>1:
     viewVideo=sys.argv[1]
@@ -26,11 +26,11 @@ marker_size=20 #cm
 
 realWorldEfficiency=.7 
 aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)
-parameters = aruco.DetectorParameters_create()
+parameters = aruco.DetectorParameters()
 
-calib_path="/calibrationfiles/"
-cameraMatrix   = np.loadtxt(calib_path+'camera_matrix.txt', delimiter=',')
-cameraDistortion   = np.loadtxt(calib_path+'distortion_coefficients.txt', delimiter=',')
+calib_path="calibrationfiles/"
+cameraMatrix   = np.loadtxt(calib_path+'cameraMatrix.txt', delimiter=',')
+cameraDistortion   = np.loadtxt(calib_path+'cameraDistortion.txt', delimiter=',')
 #############################
 
 seconds=0
@@ -53,7 +53,9 @@ while time.time()-start_time<seconds:
     frame_np = np.array(frame)
     gray_img = cv2.cvtColor(frame_np,cv2.COLOR_BGR2GRAY)
     ids=''
-    corners, ids, rejected = aruco.detectMarkers(image=gray_img,dictionary=aruco_dict,parameters=parameters)
+    detector = aruco.ArucoDetector(aruco_dict,parameters)
+    corners, ids, rejected = detector.detectMarkers(gray_img)
+    cv2.imshow('frame', frame_np)
     if ids is not None:
         print("Found these IDs in the frame:")
         print(ids)
