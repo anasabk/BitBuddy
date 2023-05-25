7456c48973eba282e32e62f443a139bf4e4c3ec3
Enes <anas.abk2001@gmail.com>
2023-05-25 12:16:43 +0300

    Update inverse_kinematics.h

--- embedded/include/inverse_kinematics.h
+++ embedded/include/inverse_kinematics.h
@@ -23,31 +23,67 @@ int def[] = {
     90, 90, 0
 };
 
-int step[][12] = {
-    {   // bring front left leg upward and forward
-        90, 60, 60,
-        80, 125, 110,
-        90, 150, 95,
-        80, 0, 50
+int step_offset[][12] = {
+    // {   // bring left front leg upward and forward
+    //     90, 60, 60,
+    //     80, 125, 110,
+    //     90, 150, 95,
+    //     80, 0, 50
+    // },
+    {   // bring left front leg upward and forward
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0,
+        0, 40, -35
     },
+    // {   // move body forward and bring front left leg to the ground
+    //     90, 60, 90,
+    //     80, 120, 90,
+    //     90, 135, 95,
+    //     80, 0, 115
+    // },
     {   // move body forward and bring front left leg to the ground
-        90, 60, 90,
-        80, 125, 80,
-        90, 150, 95,
-        80, 0, 115
+        0,  5,   20,
+        0, -5,  -20,
+        0, -15,  0,
+        0, -40,  35
     },
-    {   // move body forward and bring front left leg to the ground
-        90, 60, 90,
-        80, 150, 130,
-        90, 150, 95,
-        80, 0, 115
+    // {   // bring right back leg upward and forward
+    //     90, 60, 90,
+    //     80, 150, 130,
+    //     90, 150, 95,
+    //     80, 0, 115
+    // },
+    {   // bring right back leg upward and forward
+        0,  0,   0,
+        0,  30,  40,
+        0,  0,   0,
+        0,  0,   0
     },
-    {   // move body forward and bring front left leg to the ground
-        90, 60, 90,
-        80, 150, 130,
-        90, 150, 95,
-        80, 0, 115
+    // {   // move body forward and bring right back leg to the ground
+    //     90, 60, 90,
+    //     80, 133, 50,
+    //     90, 150, 95,
+    //     80, 0, 115
+    // },
+    {   // move body forward and bring right back leg to the ground
+        0, -13,  40,
+        0, -30, -40,
+        0, -5,  -10,
+        0,  15,  0
     },
+    /*  back
+        4   5
+        125 110
+        120 90  -5  -20
+        133 50  +13 -40
+    */
+    /*  front
+        7   8
+        150 95  
+        135 95  -15 0
+        130 85  -5  -10
+    */
 };
 
 
