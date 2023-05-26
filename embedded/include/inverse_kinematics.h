dc85e3a52ee8efe5a0e7e8f80eaa9d04c40784d6
Enes <anas.abk2001@gmail.com>
2023-05-26 21:49:21 +0300

    Update inverse_kinematics.h

--- embedded/include/inverse_kinematics.h
+++ embedded/include/inverse_kinematics.h
@@ -2,6 +2,9 @@
 #define INVERSE_KINEMATICS_H
 
 
+#include "CalServo.h"
+
+
 int stand[] = {
     90, 60, 60,
     80, 125, 110,
@@ -97,6 +100,57 @@ int step_offset[][12] = {
         130 85  -5  -10
     */
 };
+
+
+class Leg
+{
+private:
+    CalServo *servos[3];
+    int hip_l, l1, l2;
+
+    /**
+     * @note Left == false
+     * @note Right == true
+     */
+    bool side;
+    
+public:
+    Leg(CalServo *hip, CalServo *shoulder, CalServo *knee, int hip_l, int l1, int l2, bool side);
+    ~Leg();
+
+    bool move(int x_mm, int y_mm, int z_mm);
+};
+
+Leg::Leg(
+    CalServo *hip, 
+    CalServo *shoulder, 
+    CalServo *knee, 
+    int hip_l, 
+    int l2, 
+    int l1, 
+    bool side)
+{
+}
+
+Leg::~Leg()
+{
+}
+
+bool Leg::move(int x_mm, int y_mm, int z_mm) {
+    int R2_yz = y_mm*y_mm + z_mm*z_mm;
+    int R2_xyz = x_mm*x_mm + y_mm*y_mm + z_mm*z_mm;
+
+    int degrees[3];
+    degrees[0] = acos(hip_l/sqrt(R2_yz))*180/M_PI + 90;
+    degrees[1] = (acos((l2*l2 - l1*l1 - R2_xyz) / (2 * l1 * sqrt(R2_xyz))) - atan(x_mm / sqrt(R2_yz))) * 180 / M_PI;
+    degrees[2] = acos((R2_xyz - l2*l2 - l1*l1) / (2 * l1 * l2)) * 180 / M_PI - 35;
+
+    for(int i = 0; i < 3; i++) {
+        servos[i]->sweep(degrees[i], 3000);
+    }
+
+    return true;
+}
 
 
 #endif
