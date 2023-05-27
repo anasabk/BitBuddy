bb0ea6648999c12e7717c76f72e3e2aa6214430e
Enes <anas.abk2001@gmail.com>
2023-05-27 14:32:21 +0300

    Update CalServo.cpp

--- embedded/components/CalServo/CalServo.cpp
+++ embedded/components/CalServo/CalServo.cpp
@@ -56,12 +56,28 @@ void CalServo::sweep(int start, int dest, int dur_ms) {
         return;
     }
     
-    int dt = dur_ms / abs(dest - start);
+    int dt_ns = dur_ms / abs(dest - start) * 1000000;
     int dir = (dest - start) > 0 ? 1 : -1;
     int current = start;
+    
+    struct timespec timeNow;
+    clock_gettime(CLOCK_MONOTONIC, &timeNow);
+    
     while(current*dir < dest*dir) {
         set_degree(current);
         current += dir;
+
+        // Add dt_ns to current time
+        timeNow.tv_nsec += dt_ns; // dt_ns in nanoseconds
+
+        // Handle overflow
+        while (timeNow.tv_nsec >= 1000000000L) {
+            timeNow.tv_nsec -= 1000000000L;
+            timeNow.tv_sec++;
+        }
+
+        // Sleep until the next dt_ns point
+        clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &timeNow, nullptr);
     }
 }
 
