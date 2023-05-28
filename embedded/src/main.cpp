76c50036a6b1aa379c37582d768d08e748fe5313
Enes <anas.abk2001@gmail.com>
2023-05-28 22:49:43 +0300

    Update main.cpp

--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -179,25 +179,25 @@ extern "C" int main() {
 	legs[2].move_offset(-40, 0, 0);
 	legs[3].move_offset(-40, 0, 0);
 
-	// sleep(1);
-	// legs[3].move_offset(40, 0, -30);
-	// sleep(1);
-	// legs[3].move_offset(40, 0, 30);
-	// sleep(1);
-	// legs[0].move_offset(-40, 0, 0);
-	// legs[1].move_offset(-40, 0, 0);
-	// legs[2].move_offset(-40, 0, 0);
-	// legs[3].move_offset(-40, 0, 0);
+	sleep(1);
+	legs[3].move_offset(40, 0, -30);
+	sleep(1);
+	legs[3].move_offset(40, 0, 30);
+	sleep(1);
+	legs[0].move_offset(-40, 0, 0);
+	legs[1].move_offset(-40, 0, 0);
+	legs[2].move_offset(-40, 0, 0);
+	legs[3].move_offset(-40, 0, 0);
 
-	// sleep(1);
-	// legs[1].move_offset(40, 0, -30);
-	// sleep(1);
-	// legs[1].move_offset(40, 0, 30);
-	// sleep(1);
-	// legs[0].move_offset(-40, 0, 0);
-	// legs[1].move_offset(-40, 0, 0);
-	// legs[2].move_offset(-40, 0, 0);
-	// legs[3].move_offset(-40, 0, 0);
+	sleep(1);
+	legs[1].move_offset(40, 0, -30);
+	sleep(1);
+	legs[1].move_offset(40, 0, 30);
+	sleep(1);
+	legs[0].move_offset(-40, 0, 0);
+	legs[1].move_offset(-40, 0, 0);
+	legs[2].move_offset(-40, 0, 0);
+	legs[3].move_offset(-40, 0, 0);
 
 
 	uint8_t dest_servo = 0;
