23a7a051cd33ef393457402aab3b61000b5664ac
Enes <anas.abk2001@gmail.com>
2023-05-31 14:57:50 +0300

    Update main.cpp

--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -159,49 +159,49 @@ extern "C" int main() {
 	legs[3].move(30, 55, 170);
 
 	for(int i = 0; i < 4; i++) {
-		usleep(500000);
+		usleep(200000);
 		legs[2].move(35, 55, 130);
-		usleep(500000);
+		usleep(200000);
 		legs[2].move(60, 55, 130);
-		usleep(500000);
+		usleep(200000);
 		legs[2].move(60, 55, 170);
-		usleep(500000);
+		usleep(200000);
 		legs[0].move_offset(-25, 0, 0);
 		legs[1].move_offset(-25, 0, 0);
 		legs[2].move_offset(-25, 0, 0);
 		legs[3].move_offset(-25, 0, 0);
 
-		usleep(500000);
+		usleep(200000);
 		legs[0].move(-50, 55, 130);
-		usleep(500000);
+		usleep(200000);
 		legs[0].move(-25, 55, 130);
-		usleep(500000);
+		usleep(200000);
 		legs[0].move(-25, 55, 170);
-		usleep(500000);
+		usleep(200000);
 		legs[0].move_offset(-25, 0, 0);
 		legs[1].move_offset(-25, 0, 0);
 		legs[2].move_offset(-25, 0, 0);
 		legs[3].move_offset(-25, 0, 0);
 
-		usleep(500000);
+		usleep(20000);
 		legs[3].move(35, 55, 130);
-		usleep(500000);
+		usleep(200000);
 		legs[3].move(60, 55, 130);
-		usleep(500000);
+		usleep(200000);
 		legs[3].move(60, 55, 170);
-		usleep(500000);
+		usleep(200000);
 		legs[0].move_offset(-25, 0, 0);
 		legs[1].move_offset(-25, 0, 0);
 		legs[2].move_offset(-25, 0, 0);
 		legs[3].move_offset(-25, 0, 0);
 
-		usleep(500000);
+		usleep(200000);
 		legs[1].move(-50, 55, 130);
-		usleep(500000);
+		usleep(200000);
 		legs[1].move(-25, 55, 130);
-		usleep(500000);
+		usleep(200000);
 		legs[1].move(-25, 55, 170);
-		usleep(500000);
+		usleep(200000);
 		legs[0].move_offset(-25, 0, 0);
 		legs[1].move_offset(-25, 0, 0);
 		legs[2].move_offset(-25, 0, 0);
