b667059914eb397e99a2dc77de84b5cfd5f56a77
Enes <anas.abk2001@gmail.com>
2023-06-01 19:58:44 +0300

    Update main.cpp

--- embedded/main.cpp
+++ embedded/main.cpp
@@ -164,10 +164,10 @@ extern "C" int main() {
 	// usleep(100000);
 	// legs[2].move(0, 80, 170, 0, 0, 0);
 	// usleep(200000);
-	legs[0].move_offset(-30, 25, 0, 0, 0, 0);
-	legs[1].move_offset( 30,-25, 0, 0, 0, 0);
-	legs[2].move_offset( 30,-25, 0, 0, 0, 0);
-	legs[3].move_offset(-30, 25, 0, 0, 0, 0);
+	legs[0].move_offset(0, 25, 0, 0, 0, 0);
+	legs[1].move_offset(0,-25, 0, 0, 0, 0);
+	legs[2].move_offset(0,-25, 0, 0, 0, 0);
+	legs[3].move_offset(0, 25, 0, 0, 0, 0);
 	
 
 	// for(int i = 0; i < 20; i++) {
