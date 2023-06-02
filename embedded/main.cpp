3c0bc1c649e0db9f885ce486e7f5f31a90bc8540
Enes <anas.abk2001@gmail.com>
2023-06-02 10:49:01 +0300

    Update main.cpp

--- embedded/main.cpp
+++ embedded/main.cpp
@@ -178,10 +178,10 @@ extern "C" int main() {
 		usleep(100000);
 		legs[2].move(60, 30, 170, 0, 0, 0);
 		usleep(200000);
-		legs[0].move_offset(-25, 0, 0, 0, 0, 0);
+		legs[0].move_offset(-20, 0, 0, 0, 0, 0);
 		legs[1].move_offset(-25, 0, 0, 0, 0, 0);
 		legs[2].move_offset(-25, 0, 0, 0, 0, 0);
-		legs[3].move_offset(-25, 0, 0, 0, 0, 0);
+		legs[3].move_offset(-20, 0, 0, 0, 0, 0);
 
 		usleep(20000);
 		legs[0].move(-50, 55, 100, 0, 0, 0);
@@ -190,10 +190,10 @@ extern "C" int main() {
 		usleep(100000);
 		legs[0].move(-25, 55, 170, 0, 0, 0);
 		usleep(200000);
-		legs[0].move_offset(-25, 0, 0, 0, 0, 0);
+		legs[0].move_offset(-20, 0, 0, 0, 0, 0);
 		legs[1].move_offset(-25, 0, 0, 0, 0, 0);
 		legs[2].move_offset(-25, 0, 0, 0, 0, 0);
-		legs[3].move_offset(-25, 0, 0, 0, 0, 0);
+		legs[3].move_offset(-20, 0, 0, 0, 0, 0);
 
 		usleep(20000);
 		legs[3].move(35, 55, 100, 0, 0, 0);
@@ -202,10 +202,10 @@ extern "C" int main() {
 		usleep(100000);
 		legs[3].move(60, 55, 170, 0, 0, 0);
 		usleep(200000);
-		legs[0].move_offset(-25, 0, 0, 0, 0, 0);
+		legs[0].move_offset(-20, 0, 0, 0, 0, 0);
 		legs[1].move_offset(-25, 0, 0, 0, 0, 0);
 		legs[2].move_offset(-25, 0, 0, 0, 0, 0);
-		legs[3].move_offset(-25, 0, 0, 0, 0, 0);
+		legs[3].move_offset(-20, 0, 0, 0, 0, 0);
 
 		usleep(20000);
 		legs[1].move(-50, 55, 100, 0, 0, 0);
@@ -214,10 +214,10 @@ extern "C" int main() {
 		usleep(100000);
 		legs[1].move(-25, 55, 170, 0, 0, 0);
 		usleep(200000);
-		legs[0].move_offset(-25, 0, 0, 0, 0, 0);
+		legs[0].move_offset(-20, 0, 0, 0, 0, 0);
 		legs[1].move_offset(-25, 0, 0, 0, 0, 0);
 		legs[2].move_offset(-25, 0, 0, 0, 0, 0);
-		legs[3].move_offset(-25, 0, 0, 0, 0, 0);
+		legs[3].move_offset(-20, 0, 0, 0, 0, 0);
 	}
 
 
