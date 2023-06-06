66c758aba20674ce93c8b01e11dba32aeadf9906
Enes <anas.abk2001@gmail.com>
2023-06-07 00:22:29 +0300

    Update main.cpp

--- embedded/main.cpp
+++ embedded/main.cpp
@@ -14,7 +14,7 @@
 #include <sched.h>
 #include <linux/i2c-dev.h>
 #include <i2c/smbus.h>
-#include "components/RobotDog/include/inv_kinematics.h"
+// #include "components/RobotDog/include/inv_kinematics.h"
 
 int pwm_list[20] = {450, 550, 650, 750, 850, 950, 1050, 1150, 1250, 1350, 1450, 1550, 1650, 1750, 1850, 1950, 2050, 2150, 2250, 2350};
 
@@ -86,50 +86,50 @@ CalServo *servos_g;
 // }
 
 extern "C" int main() {
-	// RobotDog robot(1, MPU6050_DEF_I2C_ADDRESS, 1, 0x40, 1, 0x27);
-	// robot.run();
+	RobotDog robot(1, MPU6050_DEF_I2C_ADDRESS, 1, 0x40, 1, 0x27);
+	robot.run();
 	
-    PCA9685 pca(1, 0x40);
-	/** 
-	 * \verbatim
-	 * 		Numbers of servo channels
-	 * 			   of each leg
-	 * 				 | Top | Mid | Low |
-	 * 				 |  0  |  1  |  2  |
-	 * --------------|-----|-----|-----|
-	 * Front Right 0 |	6  |  7	 |	8  |
-	 * Front Left  1 |	9  |  10 |	11 |
-	 * Back Right  2 |	3  |  4	 |	5  |
-	 * Back Left   3 |	0  |  1	 |	2  |
-	 * \endverbatim
-	 */
-	CalServo servo[12] {
-		// Top, Mid, and Low motors for each leg
-		CalServo(&pca, 0), CalServo(&pca, 1), CalServo(&pca, 2),	// Back Left
-		CalServo(&pca, 3), CalServo(&pca, 4), CalServo(&pca, 5),	// Back Right
-		CalServo(&pca, 6), CalServo(&pca, 7), CalServo(&pca, 8),	// Front Right 
-		CalServo(&pca, 9), CalServo(&pca, 10), CalServo(&pca, 11)	// Front Left
-	};
+    // PCA9685 pca(1, 0x40);
+	// /** 
+	//  * \verbatim
+	//  * 		Numbers of servo channels
+	//  * 			   of each leg
+	//  * 				 | Top | Mid | Low |
+	//  * 				 |  0  |  1  |  2  |
+	//  * --------------|-----|-----|-----|
+	//  * Front Right 0 |	6  |  7	 |	8  |
+	//  * Front Left  1 |	9  |  10 |	11 |
+	//  * Back Right  2 |	3  |  4	 |	5  |
+	//  * Back Left   3 |	0  |  1	 |	2  |
+	//  * \endverbatim
+	//  */
+	// CalServo servo[12] {
+	// 	// Top, Mid, and Low motors for each leg
+	// 	CalServo(&pca, 0), CalServo(&pca, 1), CalServo(&pca, 2),	// Back Left
+	// 	CalServo(&pca, 3), CalServo(&pca, 4), CalServo(&pca, 5),	// Back Right
+	// 	CalServo(&pca, 6), CalServo(&pca, 7), CalServo(&pca, 8),	// Front Right 
+	// 	CalServo(&pca, 9), CalServo(&pca, 10), CalServo(&pca, 11)	// Front Left
+	// };
 
-	Leg legs[4] {
-		Leg(&servo[0],  -4, &servo[1], 2.443508, &servo[2], -8, 55, 110, 130, false, false),
-		Leg(&servo[3],  10, &servo[4], -3.443508, &servo[5], 0, 55, 110, 130, true, false),
-		Leg(&servo[6],   0, &servo[7], -4.042452, &servo[8], 13.594, 55, 110, 130, true, true),
-		Leg(&servo[9], 8, &servo[10], 5.957548, &servo[11], 14.594, 55, 110, 130, false, true),
-	};
+	// Leg legs[4] {
+	// 	Leg(&servo[0],  -4, &servo[1], 2.443508, &servo[2], -8, 55, 110, 130, false, false),
+	// 	Leg(&servo[3],  10, &servo[4], -3.443508, &servo[5], 0, 55, 110, 130, true, false),
+	// 	Leg(&servo[6],   0, &servo[7], -4.042452, &servo[8], 13.594, 55, 110, 130, true, true),
+	// 	Leg(&servo[9], 8, &servo[10], 5.957548, &servo[11], 14.594, 55, 110, 130, false, true),
+	// };
 
-	servos_g = servo;
-	legs_g = legs;
+	// servos_g = servo;
+	// legs_g = legs;
 
-    pca.set_pwm_freq(50);
-	usleep(1000000);
+    // pca.set_pwm_freq(50);
+	// usleep(1000000);
 
-	printf("Calibrating ...\n");
+	// printf("Calibrating ...\n");
 
-	for(int i = 0; i < 12; i++)
-		servo[i].refresh_fitter(pwm_list, degree_list[servo[i].getChannel()], 20);
+	// for(int i = 0; i < 12; i++)
+	// 	servo[i].refresh_fitter(pwm_list, degree_list[servo[i].getChannel()], 20);
 
-	printf("Moving ...\nStanding ...\n");
+	// printf("Moving ...\nStanding ...\n");
 
 	// pthread_t temp;
 	// for(int i = 0; i < 12; i++) {
@@ -152,89 +152,89 @@ extern "C" int main() {
 	// legs[2].move(30, 55, 65);
 	// legs[3].move(30, 55, 65);
 	
-	legs[0].move(-50, 55, 170, 0, 0, 0);
-	legs[1].move(-50, 55, 170, 0, 0, 0);
-	legs[2].move(30, 55, 170, 0, 0, 0);
-	legs[3].move(30, 55, 170, 0, 0, 0);
-	sleep(4);
-	usleep(20000);
-	legs[2].move(0, 55, 100, 0, 0, 0);
-	usleep(20000);
-	legs[2].move(0, 30, 100, 0, 0, 0);
-	usleep(100000);
-	legs[2].move(0, 30, 170, 0, 0, 0);
-	usleep(200000);
-	legs[0].move_offset(-30, -25, 0, 0, 0, 0);
-	legs[1].move_offset( 30,  25, 0, 0, 0, 0);
-	legs[2].move_offset( 30,  25, 0, 0, 0, 0);
-	legs[3].move_offset(-30, -25, 0, 0, 0, 0);
+	// legs[0].move(-50, 55, 170, 0, 0, 0);
+	// legs[1].move(-50, 55, 170, 0, 0, 0);
+	// legs[2].move(30, 55, 170, 0, 0, 0);
+	// legs[3].move(30, 55, 170, 0, 0, 0);
+	// sleep(4);
+	// usleep(20000);
+	// legs[2].move(0, 55, 100, 0, 0, 0);
+	// usleep(20000);
+	// legs[2].move(0, 30, 100, 0, 0, 0);
+	// usleep(100000);
+	// legs[2].move(0, 30, 170, 0, 0, 0);
+	// usleep(200000);
+	// legs[0].move_offset(-30, -25, 0, 0, 0, 0);
+	// legs[1].move_offset( 30,  25, 0, 0, 0, 0);
+	// legs[2].move_offset( 30,  25, 0, 0, 0, 0);
+	// legs[3].move_offset(-30, -25, 0, 0, 0, 0);
 	
 
-	for(int i = 0; i < 20; i++) {
-		usleep(20000);
-		legs[2].move(035, 55, 100, 0, 0, 0);
-		usleep(20000);
-		legs[2].move(60, 30, 100, 0, 0, 0);
-		usleep(100000);
-		legs[2].move(60, 30, 170, 0, 0, 0);
-		usleep(200000);
-		legs[0].move_offset(-20, 0, 0, 0, 0, 0);
-		legs[1].move_offset(-25, 0, 0, 0, 0, 0);
-		legs[2].move_offset(-25, 0, 0, 0, 0, 0);
-		legs[3].move_offset(-20, 0, 0, 0, 0, 0);
+	// for(int i = 0; i < 20; i++) {
+	// 	usleep(20000);
+	// 	legs[2].move(035, 55, 100, 0, 0, 0);
+	// 	usleep(20000);
+	// 	legs[2].move(60, 30, 100, 0, 0, 0);
+	// 	usleep(100000);
+	// 	legs[2].move(60, 30, 170, 0, 0, 0);
+	// 	usleep(200000);
+	// 	legs[0].move_offset(-25, 0, 0, 0, 0, 0);
+	// 	legs[1].move_offset(-25, 0, 0, 0, 0, 0);
+	// 	legs[2].move_offset(-25, 0, 0, 0, 0, 0);
+	// 	legs[3].move_offset(-25, 0, 0, 0, 0, 0);
 
-		usleep(20000);
-		legs[0].move(-50, 55, 100, 0, 0, 0);
-		usleep(20000);
-		legs[0].move(-25, 55, 100, 0, 0, 0);
-		usleep(100000);
-		legs[0].move(-25, 55, 170, 0, 0, 0);
-		usleep(200000);
-		legs[0].move_offset(-20, 0, 0, 0, 0, 0);
-		legs[1].move_offset(-25, 0, 0, 0, 0, 0);
-		legs[2].move_offset(-25, 0, 0, 0, 0, 0);
-		legs[3].move_offset(-20, 0, 0, 0, 0, 0);
+	// 	usleep(20000);
+	// 	legs[0].move(-50, 55, 100, 0, 0, 0);
+	// 	usleep(20000);
+	// 	legs[0].move(-25, 55, 100, 0, 0, 0);
+	// 	usleep(100000);
+	// 	legs[0].move(-25, 55, 170, 0, 0, 0);
+	// 	usleep(200000);
+	// 	legs[0].move_offset(-25, 0, 0, 0, 0, 0);
+	// 	legs[1].move_offset(-25, 0, 0, 0, 0, 0);
+	// 	legs[2].move_offset(-25, 0, 0, 0, 0, 0);
+	// 	legs[3].move_offset(-25, 0, 0, 0, 0, 0);
 
-		usleep(20000);
-		legs[3].move(35, 55, 100, 0, 0, 0);
-		usleep(20000);
-		legs[3].move(60, 55, 100, 0, 0, 0);
-		usleep(100000);
-		legs[3].move(60, 55, 170, 0, 0, 0);
-		usleep(200000);
-		legs[0].move_offset(-20, 0, 0, 0, 0, 0);
-		legs[1].move_offset(-25, 0, 0, 0, 0, 0);
-		legs[2].move_offset(-25, 0, 0, 0, 0, 0);
-		legs[3].move_offset(-20, 0, 0, 0, 0, 0);
+	// 	usleep(20000);
+	// 	legs[3].move(35, 55, 100, 0, 0, 0);
+	// 	usleep(20000);
+	// 	legs[3].move(60, 55, 100, 0, 0, 0);
+	// 	usleep(100000);
+	// 	legs[3].move(60, 55, 170, 0, 0, 0);
+	// 	usleep(200000);
+	// 	legs[0].move_offset(-25, 0, 0, 0, 0, 0);
+	// 	legs[1].move_offset(-25, 0, 0, 0, 0, 0);
+	// 	legs[2].move_offset(-25, 0, 0, 0, 0, 0);
+	// 	legs[3].move_offset(-25, 0, 0, 0, 0, 0);
 
-		usleep(20000);
-		legs[1].move(-50, 55, 100, 0, 0, 0);
-		usleep(20000);
-		legs[1].move(-25, 55, 100, 0, 0, 0);
-		usleep(100000);
-		legs[1].move(-25, 55, 170, 0, 0, 0);
-		usleep(200000);
-		legs[0].move_offset(-20, 0, 0, 0, 0, 0);
-		legs[1].move_offset(-25, 0, 0, 0, 0, 0);
-		legs[2].move_offset(-25, 0, 0, 0, 0, 0);
-		legs[3].move_offset(-20, 0, 0, 0, 0, 0);
-	}
+	// 	usleep(20000);
+	// 	legs[1].move(-50, 55, 100, 0, 0, 0);
+	// 	usleep(20000);
+	// 	legs[1].move(-25, 55, 100, 0, 0, 0);
+	// 	usleep(100000);
+	// 	legs[1].move(-25, 55, 170, 0, 0, 0);
+	// 	usleep(200000);
+	// 	legs[0].move_offset(-25, 0, 0, 0, 0, 0);
+	// 	legs[1].move_offset(-25, 0, 0, 0, 0, 0);
+	// 	legs[2].move_offset(-25, 0, 0, 0, 0, 0);
+	// 	legs[3].move_offset(-25, 0, 0, 0, 0, 0);
+	// }
 
 
-	uint8_t dest_servo = 0;
-	int dest_degree = 0;
-    while(true) {
-		scanf("%d %d", &dest_servo, &dest_degree);
-		for(int i = 0; i < 12; i++){
-			printf("%d %d %d\n", i, servo[i].getChannel());
-			if(servo[i].getChannel() == dest_servo) {
-				servo[i].set_degree(dest_degree);
-				break;
-			}
-		}
-    }
+	// uint8_t dest_servo = 0;
+	// int dest_degree = 0;
+    // while(true) {
+	// 	scanf("%d %d", &dest_servo, &dest_degree);
+	// 	for(int i = 0; i < 12; i++){
+	// 		printf("%d %d %d\n", i, servo[i].getChannel());
+	// 		if(servo[i].getChannel() == dest_servo) {
+	// 			servo[i].set_degree(dest_degree);
+	// 			break;
+	// 		}
+	// 	}
+    // }
 
-	return 0;
+	// return 0;
 
 
 	// if (gpioInitialise() < 0) {
