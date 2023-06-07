10c3e4229b2e9698f3343e74496ff47ad695538d
Enes <anas.abk2001@gmail.com>
2023-06-07 20:53:47 +0300

    Update main.cpp

--- embedded/main.cpp
+++ embedded/main.cpp
@@ -61,10 +61,10 @@ CalServo *servos_g;
 // }
 
 extern "C" int main() {
-	// if (gpioInitialise() < 0) {
-	// 	printf("Failure...");
-	// 	exit(-1);
-	// }
+	if (gpioInitialise() < 0) {
+		printf("Failure...");
+		exit(-1);
+	}
 
 	RobotDog robot(1, MPU6050_DEF_I2C_ADDRESS, 1, 0x40, 1, 0x27);
 	robot.run();
