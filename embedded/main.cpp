efe13615b1450d0e8ecf800b8a0e86a4d72c4baf
Enes <anas.abk2001@gmail.com>
2023-06-07 11:49:44 +0300

    initialize pigpio at launch

--- embedded/main.cpp
+++ embedded/main.cpp
@@ -86,8 +86,15 @@ CalServo *servos_g;
 // }
 
 extern "C" int main() {
+	if (gpioInitialise() < 0) {
+		printf("Failure...");
+		exit(-1);
+	}
+
 	RobotDog robot(1, MPU6050_DEF_I2C_ADDRESS, 1, 0x40, 1, 0x27);
 	robot.run();
+
+	gpioTerminate();
 	
     // PCA9685 pca(1, 0x40);
 	// /** 
