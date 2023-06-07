c35b9d7604dfd429339b166d8194a1ca1b64f631
Enes <anas.abk2001@gmail.com>
2023-06-07 15:41:25 +0300

    Update RobotDog.cpp

--- embedded/components/RobotDog/src/RobotDog.cpp
+++ embedded/components/RobotDog/src/RobotDog.cpp
@@ -11,9 +11,9 @@ RobotDog::RobotDog(int mpu_bus, int mpu_addr, int pca_bus, int pca_addr, int lcd
 		CalServo(&pca, 0), CalServo(&pca, 1), CalServo(&pca, 2)		// Back Left
     },
     legs{
-        Leg(&servos[0],-4, &servos[1],  2.443508, &servos[2],     -8, 55, 110, 130, false, false),
+        Leg(&servos[0],-4, &servos[1],  2.443508, &servos[2],     1, 55, 110, 130, false, false),
         Leg(&servos[3],10, &servos[4], -3.443508, &servos[5],      0, 55, 110, 130,  true, false),
-        Leg(&servos[6], 0, &servos[7], -4.042452, &servos[8],  4., 55, 110, 130,  true, true),
+        Leg(&servos[6], 0, &servos[7], -4.042452, &servos[8], 13.594, 55, 110, 130,  true, true),
         Leg(&servos[9], 8, &servos[10], 5.957548, &servos[11],14.594, 55, 110, 130, false, true),
 	},
     main_body(&legs[Body::LEFTFRONT], &legs[Body::RIGHTFRONT], &legs[Body::LEFTBACK], &legs[Body::RIGHTBACK], 185, 77.5)
