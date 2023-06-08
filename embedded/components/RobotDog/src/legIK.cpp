417b1a6a076f1ceeb92d4418f70ff33ed438ab44
Enes <anas.abk2001@gmail.com>
2023-06-08 23:49:47 +0300

    Update legIK.cpp

--- embedded/components/RobotDog/src/legIK.cpp
+++ embedded/components/RobotDog/src/legIK.cpp
@@ -88,7 +88,7 @@ void Leg::get_degree(double x_mm, double y_mm, double z_mm, int *theta1, int *th
     
     double R2_yz = pow(y_mm, 2) + pow(z_mm, 2);
     double temp_theta = acos((l2*l2 - l1*l1 - foot_to_shoulder_sq) / (-2 * l1 * sqrt(foot_to_shoulder_sq)));
-    double hip_dir = is_right ? 1 : -1;
+    double hip_dir = is_right ? -1 : 1;
 
     double degrees[3];
     degrees[0] = (acos(hip_dir*hip_l / sqrt(R2_yz)) + atan(y_mm / fabs(z_mm)))*180/M_PI;
@@ -108,8 +108,6 @@ void Leg::get_degree(double x_mm, double y_mm, double z_mm, int *theta1, int *th
 
     if(!is_front)
         degrees[0] = 180 - degrees[0];
-
-    printf("%lf %lf %lf\n", degrees[0], degrees[1], degrees[2]);
 
     *theta1 = degrees[0] + offsets[0];
     *theta2 = degrees[1] + offsets[1];
