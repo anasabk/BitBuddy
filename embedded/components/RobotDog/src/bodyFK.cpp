cb522ebe2fa6fca57be0ea75f7b968d722fdf395
Enes <anas.abk2001@gmail.com>
2023-06-09 02:17:00 +0300

    Update bodyFK.cpp

--- embedded/components/RobotDog/src/bodyFK.cpp
+++ embedded/components/RobotDog/src/bodyFK.cpp
@@ -348,7 +348,7 @@ void Body::step_forward() {
     struct timespec timeNow;
     clock_gettime(CLOCK_MONOTONIC, &timeNow);
 
-    pose(0, 0, 0, 0, -20, 170);
+    pose(0, 0, 0, 0, -30, 170);
     timeNow.tv_nsec += 1000000000;
     while (timeNow.tv_nsec >= 1000000000L) {
         timeNow.tv_nsec -= 1000000000L;
@@ -384,7 +384,7 @@ void Body::step_forward() {
     clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &timeNow, nullptr);
 
     
-    pose(0, 0, 0, 0, 20, 170);
+    pose(0, 0, 0, 0, 30, 170);
     timeNow.tv_nsec += 1000000000;
     while (timeNow.tv_nsec >= 1000000000L) {
         timeNow.tv_nsec -= 1000000000L;
