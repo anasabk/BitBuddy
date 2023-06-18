7f1395e9d4a1ad60f9a634f96cfce1e8a88a5959
Enes <anas.abk2001@gmail.com>
2023-06-18 22:50:34 +0300

    Update RobotDog.cpp

--- embedded/components/RobotDog/src/RobotDog.cpp
+++ embedded/components/RobotDog/src/RobotDog.cpp
@@ -70,14 +70,15 @@ void RobotDog::run() {
     main_body.stand_up();
     // sleep(2);
     // main_body.pose(M_PI/4, 0, 0, 0, 0, 170);
-    sleep(2);
-    main_body.pose(0, M_PI/6, 0, 0, 0, 170);
-    sleep(2);
-    main_body.pose(0, -M_PI/6, 0, 0, 0, 170);
-    sleep(5);
-    // main_body.pose(0, 0, M_PI/4, 0, 0, 170);
     // sleep(2);
-    // main_body.step_forward();
+    // main_body.pose(0, M_PI/6, 0, 0, 0, 170);
+    // sleep(2);
+    // main_body.pose(0, -M_PI/6, 0, 0, 0, 170);
+    // sleep(5);
+    // main_body.pose(0, 0, M_PI/4, 0, 0, 170);
+    sleep(2);
+    main_body.step_forward();
+    sleep(10);
     
     running = false;
     // Initialize and start the servo_thread
