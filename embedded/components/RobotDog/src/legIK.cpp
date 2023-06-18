30d6d7e69764c5fa3402aedb20cfcba53032b7b9
Enes <anas.abk2001@gmail.com>
2023-06-18 21:44:59 +0300

    Update legIK.cpp

--- embedded/components/RobotDog/src/legIK.cpp
+++ embedded/components/RobotDog/src/legIK.cpp
@@ -51,6 +51,7 @@ Leg::Leg(
 Leg::~Leg() {
     printf("destroying leg\n");
     running = false;
+    sleep(1);
     pthread_mutex_lock(&buf_mut);
     pthread_cond_broadcast(&buf_gate);
     pthread_mutex_unlock(&buf_mut);
