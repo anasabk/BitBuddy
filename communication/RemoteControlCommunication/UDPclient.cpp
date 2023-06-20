3f8b009a17b41f0e44a8987b5906ce4f6438918e
Emircan Demirel <e.demirel2019@gtu.edu.tr>
2023-06-20 12:03:25 +0300

    server - client message format

--- communication/RemoteControlCommunication/UDPclient.cpp
+++ communication/RemoteControlCommunication/UDPclient.cpp
@@ -4,6 +4,16 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <termios.h>
+#include <cstdint>
+
+struct Message {
+    char type;
+    uint32_t id;
+    char payload[512];
+};
+
+// Mesajın toplam boyutunu aşmaması için payload'u belirli bir boyutta sınırlıyoruz.
+static_assert(sizeof(Message) <= 512, "Message size is too large for UDP packet");
 
 int getch() {
     struct termios oldSettings, newSettings;
@@ -19,8 +29,7 @@ int getch() {
 int main() {
     int clientSocket;
     struct sockaddr_in serverAddr;
-    char message[1024];
-    std::string userInput;
+    socklen_t serverAddrSize = sizeof(serverAddr);
 
     // Create socket
     clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
@@ -33,9 +42,8 @@ int main() {
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(12345);
     serverAddr.sin_addr.s_addr = inet_addr("192.168.43.138");
-    //serverAddr.sin_addr.s_addr = inet_addr("raspberrypi");
 
-    socklen_t serverAddrSize = sizeof(serverAddr);
+    uint32_t messageId = 0;
 
     // Send message and receive response from server
     while (true) {
@@ -45,22 +53,30 @@ int main() {
         if (ch == 'q') {
             break;
         } else if (ch == 'w' || ch == 'a' || ch == 's' || ch == 'd') {
-            userInput = ch;
+            // Create message
+            Message message;
+            message.type = ch;
+            message.id = messageId++;
+            strcpy(message.payload, "Test Payload");
 
-            // Send message to server
-            sendto(clientSocket, userInput.c_str(), userInput.size(), 0, (struct sockaddr*)&serverAddr, serverAddrSize);
-
-            // Get Response
-            memset(message, 0, sizeof(message));
-            int bytesRead = recvfrom(clientSocket, message, sizeof(message), 0, NULL, NULL);
-            if (bytesRead < 0) {
-                std::cerr << "Error: Could not read Response." << std::endl;
+            // Send message
+            ssize_t bytesSent = sendto(clientSocket, &message, sizeof(message), 0, (struct sockaddr*)&serverAddr, serverAddrSize);
+            if (bytesSent < 0) {
+                std::cerr << "Error: Could not send message." << std::endl;
                 break;
             }
 
-            std::cout << "\nResponse from server: " << message << std::endl;
-        }else{
-            std::cout << "Wrong input only q w a s d!!!"<<std::endl;
+            // Receive response
+            Message response;
+            ssize_t bytesRead = recvfrom(clientSocket, &response, sizeof(response), 0, NULL, NULL);
+            if (bytesRead < 0) {
+                std::cerr << "Error: Could not read message." << std::endl;
+                break;
+            }
+
+            std::cout << "Response from server: " << response.payload << std::endl;
+        } else {
+            std::cout << "Wrong input. Only 'q', 'w', 'a', 's', 'd' are allowed!" << std::endl;
         }
     }
 
--- communication/RemoteControlCommunication/UDPserver.cpp
+++ communication/RemoteControlCommunication/UDPserver.cpp
@@ -3,12 +3,21 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <cstdint>
+
+struct Message {
+    char type;
+    uint32_t id;
+    char payload[512];
+};
+
+// Mesajın toplam boyutunu aşmaması için payload'u belirli bir boyutta sınırlıyoruz.
+static_assert(sizeof(Message) <= 512, "Message size is too large for UDP packet");
 
 int main() {
     int serverSocket;
     struct sockaddr_in serverAddr, clientAddr;
-    socklen_t clientAddrSize;
-    char buffer[1024];
+    socklen_t clientAddrSize = sizeof(clientAddr);
 
     // Create socket
     serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
@@ -32,19 +41,21 @@ int main() {
 
     // Receive message from client
     while (true) {
-        memset(buffer, 0, sizeof(buffer));
-        clientAddrSize = sizeof(clientAddr);
-        int bytesRead = recvfrom(serverSocket, buffer, sizeof(buffer), 0, (struct sockaddr*)&clientAddr, &clientAddrSize);
+        Message message;
+        ssize_t bytesRead = recvfrom(serverSocket, &message, sizeof(message), 0, (struct sockaddr*)&clientAddr, &clientAddrSize);
         if (bytesRead < 0) {
             std::cerr << "Error: Message could not be read." << std::endl;
             break;
         }
 
-        std::cout << "Message from client: " << buffer << std::endl;
+        std::cout << "Message from client: " << message.payload << std::endl;
 
-        // return response
-        std::string response = "Message received.";
-        sendto(serverSocket, response.c_str(), response.size(), 0, (struct sockaddr*)&clientAddr, clientAddrSize);
+        // Return response
+        Message response;
+        response.type = 'R'; // Response type
+        response.id = message.id; // Echo back the same message ID
+        strcpy(response.payload, "Message received.");
+        sendto(serverSocket, &response, sizeof(response), 0, (struct sockaddr*)&clientAddr, clientAddrSize);
     }
 
     // Close socket
