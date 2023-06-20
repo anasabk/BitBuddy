53698ed2e92fa806c458157545017cc347e0ebf9
Enes <anas.abk2001@gmail.com>
2023-06-20 18:10:00 +0300

    Update RobotDog.cpp

--- embedded/components/RobotDog/src/RobotDog.cpp
+++ embedded/components/RobotDog/src/RobotDog.cpp
@@ -77,12 +77,12 @@ void RobotDog::run() {
     // sleep(5);
     // main_body.pose(0, 0, M_PI/4, 0, 0, 170);
     sleep(2);
-    main_body.step_forward(0, 0);
-    main_body.step_forward(0, 0);
-    main_body.step_forward(0, 0);
-    main_body.step_forward(0, 0);
-    main_body.step_forward(0, 0);
-    main_body.step_forward(0, 0);
+    main_body.step_forward(0, 20);
+    main_body.step_forward(0, 20);
+    main_body.step_forward(0, 20);
+    main_body.step_forward(0, 20);
+    main_body.step_forward(0, 20);
+    main_body.step_forward(0, 20);
     // sleep(1);
     // main_body.pose(0, 0, 0, 0,  50, 140);
     // sleep(5);
