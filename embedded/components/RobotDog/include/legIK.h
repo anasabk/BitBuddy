de3cc6e721703bf20a1f9af1bb53c8a447425715
Enes <anas.abk2001@gmail.com>
2023-06-22 14:58:53 +0300

    Finalize leg inverse kinematics

--- embedded/components/RobotDog/include/legIK.h
+++ embedded/components/RobotDog/include/legIK.h
@@ -23,49 +23,69 @@ public:
 
     ~Leg();
 
-    void get_degree(const double (&dest)[3], double (&thetas)[3]);
-    void get_degree(double x_mm, double y_mm, double z_mm, double *theta1, double *theta2, double *theta3);
+    void get_degree(const double (&dest)[3], int (&thetas)[3]);
+    void get_degree(double x_mm, double y_mm, double z_mm, int *theta1, int *theta2, int *theta3);
 
-    void get_degree_offset(const double (&offset)[3], double (&thetas)[3]);
-    void get_degree_offset(double x_mm, double y_mm, double z_mm, double *theta1, double *theta2, double *theta3);
+    void get_degree_offset(const double (&offset)[3], int (&thetas)[3]);
+    void get_degree_offset(double x_mm, double y_mm, double z_mm, int *theta1, int *theta2, int *theta3);
 
-    void move(const double (&dest)[3]);
-    void move(double x_mm, double y_mm, double z_mm);
+    void move(const double (&dest)[3], int speed_ms = 200);
+    void move(double x_mm, double y_mm, double z_mm, int speed_ms = 200);
 
-    void move_offset(const double (&offset)[3]);
-    void move_offset(double x_mm, double y_mm, double z_mm);
+    void move_d(double x_mm, double y_mm, double z_mm, int speed_ms = 200);
+
+    void move_offset(const double (&offset)[3], int speed_ms = 200);
+    void move_offset(double x_mm, double y_mm, double z_mm, int speed_ms = 200);
+
+    bool is_right();
+    bool is_front();
 
 private:
     CalServo *servos[3];
-    double theta_buf[3];
     pthread_t servo_thread_id[3];
-    double offsets[3];
+    pthread_cond_t buf_gate[3];
+    pthread_mutex_t buf_mut[3];
+    int theta_buf[3];
+    int speed_buf[3];
+    int offsets[3];
+    double last_pos[3];
     double hip_l, l1, l2;
-    int last_pos[3];
     bool running;
 
     /**
      * @note Right == true
      * @note Left == false
      */
-    bool is_right;
+    bool is_right_flag;
 
     /**
      * @note Right == true
      * @note Left == false
      */
-    bool is_front;
+    bool is_front_flag;
 
     struct servo_param {
-        const double* theta_buf;
-        CalServo *servo;
+        const int *theta_buf;
+        const int *speed_buf;
+        CalServo *servos;
+        pthread_cond_t *buf_gate;
+        pthread_mutex_t *buf_mut;
+        bool *running;
 
         servo_param(
-            const double* theta_buf,
-            CalServo *servo) 
+            const int* theta_buf,
+            const int *speed_buf,
+            CalServo *servos,
+            pthread_cond_t *buf_gate,
+            pthread_mutex_t *buf_mut,
+            bool *running) 
         {
-            this->servo = servo;
+            this->speed_buf = speed_buf;
+            this->servos = servos;
             this->theta_buf = theta_buf;
+            this->buf_gate = buf_gate;
+            this->buf_mut = buf_mut;
+            this->running = running;
         }
     };
 
--- embedded/components/RobotDog/src/legIK.cpp
+++ embedded/components/RobotDog/src/legIK.cpp
@@ -13,12 +13,20 @@ Leg::Leg(
     double hip_l, 
     double l1, 
     double l2, 
-    bool is_right,
-    bool is_front)
+    bool is_right_flag,
+    bool is_front_flag)
 {
     servos[0] = hip;
     servos[1] = shoulder;
     servos[2] = knee;
+
+    theta_buf[0] = 0;
+    theta_buf[1] = 0;
+    theta_buf[2] = 0;
+
+    speed_buf[0] = 0;
+    speed_buf[1] = 0;
+    speed_buf[2] = 0;
 
     offsets[0] = off_hip;
     offsets[1] = off_shld;
@@ -28,34 +36,43 @@ Leg::Leg(
     this->l1 = l1;
     this->l2 = l2;
 
-    this->is_right = is_right;
-    this->is_front = is_front;
+    this->is_right_flag = is_right_flag;
+    this->is_front_flag = is_front_flag;
 
     this->running = true;
 
+    buf_gate[0] = PTHREAD_COND_INITIALIZER;
+    buf_gate[1] = PTHREAD_COND_INITIALIZER;
+    buf_gate[2] = PTHREAD_COND_INITIALIZER;
+    buf_mut[0] = PTHREAD_MUTEX_INITIALIZER;
+    buf_mut[1] = PTHREAD_MUTEX_INITIALIZER;
+    buf_mut[2] = PTHREAD_MUTEX_INITIALIZER;
+
     pthread_create(
-        &servo_thread_id[0], 
+        servo_thread_id, 
         NULL, 
         servo_thread, 
-        new struct servo_param(&theta_buf[0], servos[0])
+        new struct servo_param(theta_buf, speed_buf, servos[0], buf_gate, buf_mut, &running)
     );
 
     pthread_create(
         &servo_thread_id[1], 
         NULL, 
         servo_thread, 
-        new struct servo_param(&theta_buf[1], servos[1])
+        new struct servo_param(&theta_buf[1], &speed_buf[1], servos[1], &buf_gate[1], &buf_mut[1], &running)
     );
 
     pthread_create(
         &servo_thread_id[2], 
         NULL, 
         servo_thread, 
-        new struct servo_param(&theta_buf[2], servos[2])
+        new struct servo_param(&theta_buf[2], &speed_buf[2], servos[2], &buf_gate[2], &buf_mut[2], &running)
     );
 }
 
 Leg::~Leg() {
+    printf("destroying leg\n");
+    running = false;
     pthread_kill(servo_thread_id[0], SIGTERM);
     pthread_join(servo_thread_id[0], NULL);
     pthread_kill(servo_thread_id[1], SIGTERM);
@@ -64,83 +81,175 @@ Leg::~Leg() {
     pthread_join(servo_thread_id[2], NULL);
 }
 
-void Leg::get_degree(double x_mm, double y_mm, double z_mm, double *theta1, double *theta2, double *theta3) {
-    double R2_yz = pow(y_mm, 2) + pow(z_mm, 2);
+void Leg::get_degree(double x_mm, double y_mm, double z_mm, int *theta1, int *theta2, int *theta3) {
     double foot_to_shoulder_sq = pow(x_mm, 2) + pow(y_mm, 2) + pow(z_mm, 2) - pow(hip_l, 2);
+    
+    if (foot_to_shoulder_sq < (l1*l1 + l2*l2 - 2*l1*l2*cos(35/180*M_PI)) || 
+        foot_to_shoulder_sq > pow(l1+l2, 2)) {
+        printf("Length of %lfmm from foot to shoulder is not possible, aborting ...\n", foot_to_shoulder_sq);
+        return;
+    }
+    
+    double R2_yz = pow(y_mm, 2) + pow(z_mm, 2);
     double temp_theta = acos((l2*l2 - l1*l1 - foot_to_shoulder_sq) / (-2 * l1 * sqrt(foot_to_shoulder_sq)));
+    double hip_dir = is_right_flag ? -1 : 1;
 
     double degrees[3];
-    degrees[0] = (acos(hip_l / sqrt(R2_yz)) + atan(y_mm / z_mm))*180/M_PI + offsets[0];
-    degrees[1] = (temp_theta - atan(x_mm / sqrt(R2_yz - hip_l*hip_l)))*180/M_PI + offsets[1];
-    degrees[2] = acos((foot_to_shoulder_sq - l2*l2 - l1*l1) / (-2 * l1 * l2))*180/M_PI - 35 + offsets[2];
+    degrees[0] = (acos(hip_dir*hip_l / sqrt(R2_yz)) + atan(y_mm / fabs(z_mm)))*180/M_PI;
+    degrees[1] = (temp_theta - atan(x_mm / sqrt(R2_yz - hip_l*hip_l)))*180/M_PI;
+    degrees[2] = acos((foot_to_shoulder_sq - l2*l2 - l1*l1) / (-2 * l1 * l2))*180/M_PI - 35;
 
-    if(is_right) {
+    if (degrees[0] > 180 || degrees[0] < 0 ||
+        degrees[1] > 180 || degrees[1] < 0 ||
+        degrees[2] > 180 || degrees[2] < 0) {
+        printf("Out of reach, aborting ...\n");
+    }
+
+    if(is_right_flag) {
         degrees[1] = 180 - degrees[1];
         degrees[2] = 180 - degrees[2];
     }
 
-    if(is_right != is_front)
+    if(is_front_flag)
         degrees[0] = 180 - degrees[0];
 
-    *theta1 = degrees[0];
-    *theta2 = degrees[1];
-    *theta3 = degrees[2];
+    *theta1 = degrees[0] + offsets[0];
+    *theta2 = degrees[1] + offsets[1];
+    *theta3 = degrees[2] + offsets[2];
 
     last_pos[0] = x_mm;
     last_pos[1] = y_mm;
     last_pos[2] = z_mm;
 }
 
-void Leg::get_degree(const double (&dest)[3], double (&thetas)[3]) {
+void Leg::get_degree(const double (&dest)[3], int (&thetas)[3]) {
     get_degree(dest[0], dest[1], dest[2], &thetas[0], &thetas[1], &thetas[2]);
 }
 
-void Leg::get_degree_offset(double x_mm, double y_mm, double z_mm, double *x_deg, double *y_deg, double *z_deg) {
+void Leg::get_degree_offset(double x_mm, double y_mm, double z_mm, int *x_deg, int *y_deg, int *z_deg) {
     if(last_pos[0] == -1 || last_pos[0] == -1 || last_pos[0] == -1)
         return;
 
     get_degree(last_pos[0] + x_mm, last_pos[1] + y_mm, last_pos[2] + z_mm, x_deg, y_deg, z_deg);
 }
 
-void Leg::get_degree_offset(const double (&offset)[3], double (&thetas)[3]) {
+void Leg::get_degree_offset(const double (&offset)[3], int (&thetas)[3]) {
     get_degree_offset(offset[0], offset[1], offset[2], &thetas[0], &thetas[1], &thetas[2]);
 }
 
-void Leg::move(double x_mm, double y_mm, double z_mm) {
+void Leg::move(double x_mm, double y_mm, double z_mm, int speed_ms) {
+    pthread_mutex_lock(&buf_mut[0]);
+    pthread_mutex_lock(&buf_mut[1]);
+    pthread_mutex_lock(&buf_mut[2]);
+    
     get_degree(x_mm ,y_mm, z_mm, &theta_buf[0], &theta_buf[1], &theta_buf[2]);
+    speed_buf[0] = speed_ms;
+    speed_buf[1] = speed_ms;
+    speed_buf[2] = speed_ms;
+
+    printf("degrees: %d %d %d\n", theta_buf[0], theta_buf[1], theta_buf[2]);
+    
     pthread_kill(servo_thread_id[0], SIGCONT);
     pthread_kill(servo_thread_id[1], SIGCONT);
     pthread_kill(servo_thread_id[2], SIGCONT);
+    
+    pthread_mutex_unlock(&buf_mut[0]);
+    pthread_mutex_unlock(&buf_mut[1]);
+    pthread_mutex_unlock(&buf_mut[2]);
 }
 
-void Leg::move(const double (&dest)[3]) {
+void Leg::move(const double (&dest)[3], int speed_ms) {
     move(dest[0], dest[1], dest[2]);
 }
 
-void Leg::move_offset(double x_mm, double y_mm, double z_mm) {
+void Leg::move_offset(double x_mm, double y_mm, double z_mm, int speed_ms) {
+    pthread_mutex_lock(&buf_mut[0]);
+    pthread_mutex_lock(&buf_mut[1]);
+    pthread_mutex_lock(&buf_mut[2]);
+
     get_degree_offset(x_mm ,y_mm, z_mm, &theta_buf[0], &theta_buf[1], &theta_buf[2]);
+    speed_buf[0] = speed_ms;
+    speed_buf[1] = speed_ms;
+    speed_buf[2] = speed_ms;
+    
+    printf("degrees: %d %d %d\n", theta_buf[0], theta_buf[1], theta_buf[2]);
+    
     pthread_kill(servo_thread_id[0], SIGCONT);
     pthread_kill(servo_thread_id[1], SIGCONT);
     pthread_kill(servo_thread_id[2], SIGCONT);
+    
+    pthread_mutex_unlock(&buf_mut[0]);
+    pthread_mutex_unlock(&buf_mut[1]);
+    pthread_mutex_unlock(&buf_mut[2]);
 }
 
-void Leg::move_offset(const double (&offset)[3]) {
-    move_offset(offset[0], offset[1], offset[2]);
+void Leg::move_offset(const double (&offset)[3], int speed_ms) {
+    move_offset(offset[0], offset[1], offset[2], speed_ms);
+}
+
+void Leg::move_d(double theta1, double theta2, double theta3, int speed_ms) {
+    pthread_mutex_lock(&buf_mut[0]);
+    pthread_mutex_lock(&buf_mut[1]);
+    pthread_mutex_lock(&buf_mut[2]);
+    
+    theta_buf[0] = theta1;
+    theta_buf[1] = theta2;
+    theta_buf[2] = theta3;
+
+    speed_buf[0] = speed_ms;
+    speed_buf[1] = speed_ms;
+    speed_buf[2] = speed_ms;
+    
+    printf("degrees: %d %d %d\n", theta_buf[0], theta_buf[1], theta_buf[2]);
+    
+    pthread_kill(servo_thread_id[0], SIGCONT);
+    pthread_kill(servo_thread_id[1], SIGCONT);
+    pthread_kill(servo_thread_id[2], SIGCONT);
+    
+    pthread_mutex_unlock(&buf_mut[0]);
+    pthread_mutex_unlock(&buf_mut[1]);
+    pthread_mutex_unlock(&buf_mut[2]);
+}
+
+void handler(int sig) {
+
 }
 
 void* Leg::servo_thread(void* param) {
-    const double *buffer = ((struct servo_param*)param)->theta_buf;
-    CalServo *servo = ((struct servo_param*)param)->servo;
+    printf("Entered servo thread\n");
+    const int *theta_buf = ((struct servo_param*)param)->theta_buf;
+    const int *speed_buf = ((struct servo_param*)param)->speed_buf;
+    CalServo *servo = ((struct servo_param*)param)->servos;
+    pthread_cond_t *gate = ((struct servo_param*)param)->buf_gate;
+    pthread_mutex_t *mut = ((struct servo_param*)param)->buf_mut;
+    bool *running = ((struct servo_param*)param)->running;
 
     sigset_t set;
     sigemptyset(&set);
     sigaddset(&set, SIGCONT);
     sigaddset(&set, SIGTERM);
+    pthread_sigmask(SIG_BLOCK, &set, NULL);
+
     int sig;
+    while(sigwait(&set, &sig) == 0 && sig != SIGTERM) {
+        pthread_mutex_lock(mut);
 
-    while(sigwait(&set, &sig) == 0 && sig != SIGTERM)
-        servo->sweep(*buffer, 700);
+        servo->sweep(*theta_buf, *speed_buf);
 
+        printf("Moving Servo %d %d degrees\n", servo->getChannel(), theta_buf[0]);
+        pthread_mutex_unlock(mut);
+    }
+
+    printf("Exitting servo thread\n");
     servo->set_PWM(0);
+    delete(param);
     pthread_exit(NULL);
+}
+
+bool Leg::is_right() {
+    return is_right_flag;
+}
+
+bool Leg::is_front() {
+    return is_front_flag;
 }
