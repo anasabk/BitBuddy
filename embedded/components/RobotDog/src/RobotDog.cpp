34b2dcac7959615765c0ae9dd970e74c50dc9387
Enes <anas.abk2001@gmail.com>
2023-06-26 23:39:32 +0300

    Update RobotDog.cpp

--- embedded/components/RobotDog/src/RobotDog.cpp
+++ embedded/components/RobotDog/src/RobotDog.cpp
@@ -240,7 +240,7 @@ void RobotDog::run() {
 
                     pthread_create(&mpu_thread_id, NULL, mpu6050_thread, (void*)this);
                     pthread_create(&hcsr04_thread_id, NULL, HCSR04_thread, (void*)this);
-                    pthread_create(&control_thread_id, NULL, control_thread, (void*)this);
+                    // pthread_create(&control_thread_id, NULL, control_thread, (void*)this);
                     pthread_create(&temp, NULL, read_thread, (void*)(&this->mpu_buff));
                     
                     servos[0].set_degree(86);
