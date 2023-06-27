1f6c231936b7af77be7f5bc2aa3084d009b4e047
Enes <anas.abk2001@gmail.com>
2023-06-27 12:27:49 +0300

    Update bodyFK.cpp

--- embedded/components/RobotDog/src/bodyFK.cpp
+++ embedded/components/RobotDog/src/bodyFK.cpp
@@ -414,7 +414,7 @@ void* Body::move_thread(void *param) {
     int leg_num = 0;
     int pause_counter = 0;
     while(*run_flag) {
-        printf("%f %f\n", fabs(*speed), fabs(*rot_rad));
+        printf("%f %f %d\n", fabs(*speed), fabs(*rot_rad), pause_counter);
         if((fabs(*speed) + fabs(*rot_rad)) < 0.0001)
             pause_counter++;
         else
