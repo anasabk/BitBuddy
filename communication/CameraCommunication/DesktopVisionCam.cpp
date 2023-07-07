72b2e3ea378e2da1c5f782f57a5a522dcb9d5587
Enes <anas.abk2001@gmail.com>
2023-07-07 18:31:35 +0300

    Update DesktopVisionCam.cpp

--- communication/CameraCommunication/DesktopVisionCam.cpp
+++ communication/CameraCommunication/DesktopVisionCam.cpp
@@ -1,9 +1,9 @@
-#include <opencv2/opencv.hpp>
+#include <opencv4/opencv2/opencv.hpp>
 #include <iostream>
 
 int main() {
     // Raspberry Pi'nin IP adresini ve portunu belirtin
-    cv::VideoCapture cap("http://192.168.1.103:8080"); 
+    cv::VideoCapture cap("udp://@:8083"); 
 
     if (!cap.isOpened()) {
         std::cerr << "Failed to open video stream" << std::endl;
@@ -17,7 +17,7 @@ int main() {
         cap.read(frame);
         // cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB);
 
-        cv::resize(frame, frame, cv::Size(640, 480));
+        // cv::resize(frame, frame, cv::Size(640, 480));
         cv::imshow("Raspberry Pi camera", frame);
 
         if (cv::waitKey(1) >= 0) break;
