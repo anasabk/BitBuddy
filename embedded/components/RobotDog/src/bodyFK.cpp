75609bc2c628ceffdd097505cdaed22bf58164e5
Enes <anas.abk2001@gmail.com>
2023-07-16 22:18:49 +0300

    make recovery recovery mode more aggressive

--- embedded/components/RobotDog/src/bodyFK.cpp
+++ embedded/components/RobotDog/src/bodyFK.cpp
@@ -366,12 +366,12 @@ void Body::recover() {
     servo_buf[9] = M_PI/2, servo_buf[10] = M_PI, servo_buf[11] =    0;
     move(700);
 
-    servo_buf[0] = 150*M_PI/180, servo_buf[1]  = M_PI, servo_buf[2]  = 0;
-    servo_buf[9] =       M_PI/6, servo_buf[10] = M_PI, servo_buf[11] = 0;
+    servo_buf[0] = 160*M_PI/180, servo_buf[1]  = M_PI, servo_buf[2]  = 0;
+    servo_buf[9] =       M_PI/9, servo_buf[10] = M_PI, servo_buf[11] = 0;
     move(700);
 
-    servo_buf[0] =       M_PI/6, servo_buf[1]  = M_PI, servo_buf[2]  = 0;
-    servo_buf[9] = 150*M_PI/180, servo_buf[10] = M_PI, servo_buf[11] = 0;
+    servo_buf[0] =       M_PI/9, servo_buf[1]  = M_PI, servo_buf[2]  = 0;
+    servo_buf[9] = 160*M_PI/180, servo_buf[10] = M_PI, servo_buf[11] = 0;
     move(700);
 
     clock_gettime(CLOCK_MONOTONIC, &time);
